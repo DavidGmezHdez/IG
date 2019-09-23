3b853087fbe25d99b008bf2fa071ed8bc6c7d7de
DavidGmezHdez <davidgmezhdez@gmail.com>
2019-09-23 15:34:19 +0200

    Añadido el chess

--- código/object3d.cc
+++ código/object3d.cc
@@ -57,4 +57,23 @@ void _object3D::draw_fill()
 void _object3D::draw_chess()
 {
   
+  glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+  glBegin(GL_TRIANGLES);
+  for (unsigned int i=0;i<Triangles.size();i++){
+
+    if(i%2 == 0){
+      glColor3fv((GLfloat *) &BLUE);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._0]);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._1]);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._2]);
+    }
+    else
+    {
+      glColor3fv((GLfloat *) &YEllOW);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._0]);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._1]);
+      glVertex3fv((GLfloat *) &Vertices[Triangles[i]._2]);
+    }
+  }
+  glEnd(); 
 }
