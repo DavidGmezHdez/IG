cb5b21e75836fa33ff05dbb5bf03c870a94d67ab
DavidGmezHdez <davidgmezhdez@gmail.com>
2019-12-13 13:22:02 +0100

    Modificando funciones camara

--- Práctica 1/camara.cc
+++ Práctica 1/camara.cc
@@ -15,20 +15,20 @@ Camara::Camara(Tupla3f e, Tupla3f a, Tupla3f u, int t, float f, float as, float 
 }
 
 
-void Camara::rotarXExaminar(float angulo);
-void Camara::rotarYExaminar(float angulo);
-void Camara::rotarZExaminar(float angulo);
+void Camara::rotarXExaminar(float angulo){}
+void Camara::rotarYExaminar(float angulo){}
+void Camara::rotarZExaminar(float angulo){}
         
-void Camara::rotarXPrimeraPersona(float angulo);
-void Camara::rotarYPrimeraPersona(float angulo);
-void Camara::rotarZPrimeraPersona(float angulo);
+void Camara::rotarXPrimeraPersona(float angulo){}
+void Camara::rotarYPrimeraPersona(float angulo){}
+void Camara::rotarZPrimeraPersona(float angulo){}
 
 void Camara::mover(float x, float y, float z){
     this->eye(0) = x;
     this->eye(1) = y;
     this->eye(2) = z;
 }
-void Camara::zoom(float factor);
+void Camara::zoom(float factor){}
         
 void Camara::setObserver(){
     gluLookAt(eye(0),eye(1),eye(2), at(0),at(1),at(2), up(0),up(1),up(2));
